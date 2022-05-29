75f84eb8a2f21967e2500105a727a8590433937e
baishiaihui <baishiaihui@gmail.com>
2022-05-30 00:30:12 +0900

    create search

--- app/controllers/api/v1/sort_infos_controller.rb
+++ app/controllers/api/v1/sort_infos_controller.rb
@@ -3,4 +3,15 @@ class Api::V1::SortInfosController < ApplicationController
     sortinfos = SortInfo.all
     render json: sortinfos.as_json(only: [:id, :name, :kana, :sort])
   end
+
+  # 検索
+  def search
+    data = {}
+    if params[:keyword].present?
+      data[:sortinfos] = SortInfo.search(params[:keyword])
+      render json: data
+    else
+      render json:{ message: '検索キーワードがありません'}
+    end
+  end  
 end
--- app/models/sort_info.rb
+++ app/models/sort_info.rb
@@ -1,2 +1,5 @@
 class SortInfo < ApplicationRecord
+    def self.search(keyword)
+        where(["name like? OR kana like?", "%#{keyword}%","%#{keyword}%"])
+    end
 end
